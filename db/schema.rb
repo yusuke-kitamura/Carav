869e2d19e3ce1688090fb25558830e0bdfc6d0c3
vagrant <vagrant@localhost.localdomain>
2020-04-01 08:54:37 +0000

    [start]4-2

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_04_01_082801) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.string "category"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
